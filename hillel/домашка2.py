25adff742ba0c8af6dd2e77b1c72cd44482798c1
Andrey <titorenko071@gmail.com>
2025-06-04 20:24:18 +0300

    Add files via upload

--- hillel/домашка2.py
+++ hillel/домашка2.py
@@ -1,13 +1,5 @@
-num = int(input("Введите 5-значное число: "))
 
-if 10000 <= num <= 99999:
-    reversed_num = 0
-
-    while num > 0:
-        digit = num % 10
-        reversed_num = reversed_num * 10 + digit
-        num //= 10
-
-    print("Число в обратном порядке:", reversed_num)
-else:
-    print("Это не 5-значное число!")
+s = [10, 11, 12, 13, 14, 15]
+print(s)
+s[0], s[-1] = s[-1], s[0]
+print(s)
